e92a371372304e00ffa5a7d0e2cc6aa86f404f92
James Byars <jamesbyars@g.harvard.edu>
2017-07-30 12:20:37 -0500

    Create base class & application.properties

---
+++ src/main/java/com/ernesttech/roundhousekick/DatabaseRoundhouseKickApplication.java
@@ -0,0 +1,15 @@
+package com.ernesttech.roundhousekick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class DatabaseRoundhouseKickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseRoundhouseKickApplication.class, args);
+    }
+
+}
